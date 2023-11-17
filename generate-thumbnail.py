fb7eaaffe4ed72383a7fa67b96bde4b151fcbbb0
l-eg116 <legcom6@gmail.com>
2023-11-17 17:32:48 +0100

    Added image resizing

--- generate-thumbnail.py
+++ generate-thumbnail.py
@@ -2,7 +2,10 @@
 import argparse
 import base64
 import io
+import os
 import sys
+
+from PIL import Image
 
 
 # Arguments parsing
@@ -12,8 +15,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-m', '--merge', action='store_true',
                     help="If this flag is set, the generated thumbnail will be inserted into existing gcode" +
                     "file passed as output, replacing existing thumbnail data if present.")
-parser.add_argument('-s', '--size', nargs='?', default='220*124',
-                    help="SIZE: '<int>*<int>' - Specifies the size of the generated thumbnail, 220*124 by default")
+parser.add_argument('-s', '--size', nargs='?', default='220x124',
+                    help="SIZE: '<int>x<int>' or 'keep' - Specifies the size of the generated thumbnail," +
+                    "220x124 by default. Use 'keep' to prevent image resizing.")
 parser.add_argument('input', type=argparse.FileType('rb'),
                     help="Input .png file")
 parser.add_argument('output', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
@@ -29,20 +33,31 @@ if merge:
     output_file = open(output_file.name, '+w')
     if not output_file.readable():
         raise IOError("Output should be readable to use --merge")
-try:
-    size: tuple = tuple(int(dim) for dim in args.size.strip().split('*'))
-    if len(size) != 2:
-        raise SyntaxError
-except:
-    raise SyntaxError(
-        f"SIZE should be of format '<int>*<int>', found '{args.size}'")
+resize: bool = args.size.lower() != 'keep'
+size: tuple = (220, 124)
+if resize:
+    try:
+        size = tuple(int(dim) for dim in args.size.strip().split('x'))
+        if len(size) != 2:
+            raise SyntaxError
+    except:
+        raise SyntaxError(
+            f"SIZE should be of format '<int>x<int>', found '{args.size}'")
 
 
-# Constants
+# Constants & global variables
 MAX_THUMBNAIL_LINE_LENGTH: int = 78
 
 
 # Code
+# Image resizing
+if resize:
+    image = Image.open(input_file).resize(size)
+    input_file.close()
+    input_file = io.BytesIO()  # type: ignore
+    image.save(input_file, format='png')
+    image.show()
+
 # Thumbnail generator
 thumbnail_str = base64.b64encode(input_file.read()).decode('utf-8')
 thumbnail_len = len(thumbnail_str)
@@ -50,3 +65,4 @@ thumbnail_str = "; " + "\n; ".join(thumbnail_str[i:i + MAX_THUMBNAIL_LINE_LENGTH
                                    for i in range(0, len(thumbnail_str), MAX_THUMBNAIL_LINE_LENGTH))
 thumbnail_header = f"; \n; thumbnail begin {size[0]}x{size[1]} {thumbnail_len}"
 thumbnail_footer = f"; \n; thumbnail end"
+input_file.close()
